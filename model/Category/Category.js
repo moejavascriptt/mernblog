d01262a5d6364516fe29f1d71a757fd302d4db93
moe <moe@email.com>
2025-09-11 15:33:43 -0500

    category and commment modeling

---
+++ model/Category/Category.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+// schema
+
+const categorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
+
+    shares: {
+      type: Number,
+      default: 0
+    },
+
+
+
+    posts: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+      }
+  },
+  {
+    timestamps: true
+  }
+)
+
+// compile schema to model
+
+const Category = mongoose.model('Category', categorySchema)
+
+module.exports = Category
