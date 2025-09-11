f55e84a04d74b04f5b3e7580448ae06d17e5fab8
moe <moe@email.com>
2025-09-11 01:33:48 -0500

    getting started backend | created express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const express = require('express')
+
+// !server
+
+const app = express()
+
+const server = http.createServer(app)
+// ? start the server
+
+const PORT = process.env.PORT || 9080
+server.listen(PORT, console.log(`Server is running on port ${PORT}`))
