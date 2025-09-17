dad38304c1c08dc97def4e95c047ae5d3ff143f6
moe <moe@email.com>
2025-09-17 14:20:43 -0500

    getting token from the request header

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -1,6 +1,8 @@
 const isLoggedIn = (req, res, next) => {
-  console.log('is logged in middleware')
+  console.log(req.headers)
   //get token from header
+  const token = req.headers.authorization?.split(' ')[1]
+  console.log(token)
   // verify the token
   // save the user
   // send the user
