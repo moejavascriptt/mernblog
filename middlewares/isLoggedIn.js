3fbb0c00976f59f7700acadf7a5e920fa1fcd188
moe <moe@email.com>
2025-09-17 14:30:35 -0500

    verifying token

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -1,9 +1,18 @@
+const jwt = require('jsonwebtoken')
 const isLoggedIn = (req, res, next) => {
   console.log(req.headers)
   //get token from header
   const token = req.headers.authorization?.split(' ')[1]
   console.log(token)
   // verify the token
+  jwt.verify(token, 'anykey', (err, decoded) => {
+    console.log(decoded)
+    if (err) {
+      return 'Invalid token'
+    } else {
+        return decoded
+    }
+  })
   // save the user
   // send the user
   next()
