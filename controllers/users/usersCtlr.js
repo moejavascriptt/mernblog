8c23516f69edb3b187f9eca80cc495e4805a7538
moe <moe@email.com>
2025-09-18 11:13:35 -0500

    catching async errors using the asynchandler

--- controllers/users/usersCtlr.js
+++ controllers/users/usersCtlr.js
@@ -3,108 +3,87 @@
 //@access public
 
 const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
 const User = require('../../model/User/User')
 const generateToken = require('../../utils/generateToken')
 
-exports.register = async (req, res) => {
-  console.log(req.body)
-  try {
-    // get the details
-    const { username, password, email } = req.body
-    // check if user exists
-    const user = await User.findOne({ username })
-    if (user) {
-      throw new Error('User already exists')
-    }
-    // register new user
-    const newUser = new User({
-      username,
-      email,
-      password
-    })
-    // hash password
-    const salt = await bcrypt.genSalt(10)
-    newUser.password = await bcrypt.hash(password, salt)
-    // save
-    await newUser.save()
-    res.status(201).json({
-      status: 'success',
-      message: 'User registered successfully',
-      //   _id: newUser?._id,
-      //   username: newUser?.username,
-      //   email: newUser?.email,
-      //   role: newUser?.role
-      newUser
-    })
-  } catch (error) {
-    res.json({
-      status: 'failed',
-      message: error?.message
-    })
+exports.register = asyncHandler(async (req, res) => {
+  // get the details
+  const { username, password, email } = req.body
+  // check if user exists
+  const user = await User.findOne({ username })
+  if (user) {
+    throw new Error('User already exists')
   }
-}
+  // register new user
+  const newUser = new User({
+    username,
+    email,
+    password
+  })
+  // hash password
+  const salt = await bcrypt.genSalt(10)
+  newUser.password = await bcrypt.hash(password, salt)
+  // save
+  await newUser.save()
+  res.status(201).json({
+    status: 'success',
+    message: 'User registered successfully',
+    //   _id: newUser?._id,
+    //   username: newUser?.username,
+    //   email: newUser?.email,
+    //   role: newUser?.role
+    newUser
+  })
+})
 
 // @desc login user
 //@route POST /api/v1/users/login
 //@access public
 
-exports.login = async (req, res) => {
-  try {
-    // get the login details
-    const { username, password } = req.body
-    // if user exists
-    const user = await User.findOne({ username })
-    if (!user) {
-      throw new Error('Invalid login credentials')
-    }
-    // compare the hashed password with the one the request
-    const isMatched = await bcrypt.compare(password, user?.password)
-    if (!isMatched) {
-      throw new Error('Invalid login credentials')
-    }
-
-    //update the last login
-    user.lastLogin = new Date()
-    res.json({
-      status: 'success',
-      email: user?.email,
-      _id: user?._id,
-      username: user?.username,
-      role: user?.role,
-      token: generateToken(user)
-    })
-  } catch (error) {
-    res.json({
-      status: 'failed',
-      message: error?.message
-    })
+exports.login = asyncHandler(async (req, res) => {
+  // get the login details
+  const { username, password } = req.body
+  // if user exists
+  const user = await User.findOne({ username })
+  if (!user) {
+    throw new Error('Invalid login credentials')
   }
-}
+  // compare the hashed password with the one the request
+  const isMatched = await bcrypt.compare(password, user?.password)
+  if (!isMatched) {
+    throw new Error('Invalid login credentials')
+  }
+
+  //update the last login
+  user.lastLogin = new Date()
+  res.json({
+    status: 'success',
+    email: user?.email,
+    _id: user?._id,
+    username: user?.username,
+    role: user?.role,
+    token: generateToken(user)
+  })
+})
 
 // @desc get profile
 //@route POST /api/v1/users/profile/:id
 //@access private
 
-exports.getProfile = async (req, res, next) => {
+exports.getProfile = asyncHandler(async (req, res, next) => {
   // trigger custom
   const myErr = new Error('my custom error')
   return next(myErr)
-  try {
-    // get user id from params
+  // get user id from params
 
-    const id = req.userAuth._id
-    const user = await User.findById(id)
-    console.log(user)
+  const id = req.userAuth._id
+  const user = await User.findById(id)
+  console.log(user)
 
-    res.json({
-      status: 'success',
-      message: 'Profile fetched',
-      user
-    })
-  } catch (error) {
-    res.json({
-      status: 'failed',
-      message: error?.message
-    })
-  }
-}
+  res.json({
+    status: 'success',
+    message: 'Profile fetched',
+    user
+  })
+})
