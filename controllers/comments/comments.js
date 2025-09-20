4fe1c4d3f1c9011a6db840870854b657497cd330
moe <moe@email.com>
2025-09-20 11:57:54 -0500

    fixed a lil problem wih me not getting the full message back after i sent something, also updated and deleted comment

--- controllers/comments/comments.js
+++ controllers/comments/comments.js
@@ -2,14 +2,14 @@ const asyncHandler = require('express-async-handler')
 const Comment = require('../../model/Comment/Comment')
 const Post = require('../../model/Post/Post')
 
-//@desc create a category
-//@route POST /api/v1/categoies/:postId
+//@desc create a comment
+//@route POST /api/v1/comments/:postId
 //@access Private
 
 exports.createComment = asyncHandler(async (req, res) => {
   // get the payload
-  const { message, author,} = req.body
-  //get post id from params 
+  const { message, author } = req.body
+  //get post id from params
   const postId = req.params.postId
   //* Create comment
   const comment = await Comment.create({
@@ -19,7 +19,6 @@ exports.createComment = asyncHandler(async (req, res) => {
   })
   // associate comment to a post
   await Post.findByIdAndUpdate(
-    postId,
     {
       $push: { comments: comment._id }
     },
@@ -27,7 +26,42 @@ exports.createComment = asyncHandler(async (req, res) => {
     //semd the response
     res.json({
       status: 'success',
-      message: 'Comment created successfully'
+      message: 'Comment created successfully',
+      comment
     })
   )
+})
+
+//@desc delete comment
+//@route DELETE /api/v1/comments/:id
+//@access Private
+
+exports.deleteComment = asyncHandler(async (req, res) => {
+  await Comment.findByIdAndDelete(req.params.id)
+  res.status(201).json({
+    status: 'success',
+    message: 'Comment successfully deleted'
+  })
+})
+
+//@desc update comment
+//@route PUT/ api/v1/comments/:id
+//@access Private
+
+exports.updateComment = asyncHandler(async (req, res) => {
+  const comment = await Comment.findByIdAndUpdate(
+    req.params.id,
+    {
+      message: req.body.message
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  )
+  res.status(201).json({
+    status: 'success',
+    message: 'Comment successfully updated',
+    comment
+  })
 })
--- routes/comment/commentRouter.js
+++ routes/comment/commentRouter.js
@@ -1,11 +1,20 @@
 const express = require('express')
 
-const { createComment } = require('../../controllers/comments/comments')
+const {
+  createComment,
+  deleteComment,
+  updateComment
+} = require('../../controllers/comments/comments')
 const isLoggedIn = require('../../middlewares/isLoggedIn')
 
 const commentRouter = express.Router()
 
 //create
 commentRouter.post('/:postId', isLoggedIn, createComment)
+
+//update
+commentRouter.put('/:id', isLoggedIn, updateComment)
+
+commentRouter.delete('/:id', isLoggedIn, deleteComment)
 
 module.exports = commentRouter
