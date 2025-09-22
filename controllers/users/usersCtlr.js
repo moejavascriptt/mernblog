f1a86aaff660afaa8879c796ae7851f79e07da8c
moe <moe@email.com>
2025-09-22 11:25:56 -0500

    blocking user controlller

--- controllers/users/usersCtlr.js
+++ controllers/users/usersCtlr.js
@@ -87,3 +87,40 @@ exports.getProfile = asyncHandler(async (req, res, next) => {
     user
   })
 })
+
+//@desc block user
+//@route POST /api/v1/users/block/:userIdToBloc
+//@access private
+
+exports.blockUser = asyncHandler(async (req, res) => {
+  // find the user to be blocked
+  const userIdToBlock = req.params.userIdToBlock
+  const userToBlock = await User.findById(userIdToBlock)
+  if (!userToBlock) {
+    throw new Error('User to block not folund')
+  }
+
+  // ! user who is blocking
+  const userBlocking = req.userAuth._id
+
+  // check if user is blocking him/herself
+
+  if (userIdToBlock.toString() === userBlocking.toString()) {
+    throw new Error('Cannot block yourself')
+  }
+
+  // find the current user
+  const currentUser = await User.findById(userIdToBlock)
+
+  //? Check if user already blocked
+  if (currentUser?.blockedUsers?.includes(userIdToBlock)) {
+    throw new Error('User already blocked')
+  }
+  // push the user to be blocked in the array of the current user
+  currentUser?.blockedUsers.push(userIdToBlock)
+  await currentUser.save()
+  res.json({
+    message: 'User blocked successfully',
+    status: 'success'
+  })
+})
